0b67a8a230044454b056e2e86288d9fae48cdbb8
jlyon1 <joeylyon1@gmail.com>
2017-12-23 17:37:33 -0500

    Begin adding go

---
+++ StatusCheck/src/components/CardInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tile is-child">
+  <div class="card">
+  <header class="card-header">
+  <p class="card-header-title">
+  {{url}}
+  </p>
+  </header>
+  <div class="card-content">
+  <div class="content">
+  Uptime (Last Month): {{last}}
+  </div>
+  <div class="content">
+  Live: {{good}}
+  <div class="">
+  <button class="button is-fullwidth">Visit</button>
+  </div>
+  </div>
+  </div>
+
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardInfo',
+  props: ['url', 'last', 'good'],
+  data () {
+    return {
+    }
+
+  }
+}
+</script>
---
+++ StatusCheck/src/components/InfoSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="section">
+    <div class="tile is-ancestor">
+    <div class="tile is-parent is-vertical">
+    <CardInfo url="example.com" good="true"></CardInfo>
+    <CardInfo url="example.com" good="true"></CardInfo>
+    <CardInfo url="example.com" good="true"></CardInfo>
+    <CardInfo url="example.com" good="true"></CardInfo>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardInfo from './CardInfo.vue'
+
+export default {
+  name: 'InfoSection',
+  components:{
+     'CardInfo':CardInfo
+    },
+  data () {
+    return {
+      test: 'Test!'
+    }
+  }
+}
+</script>
